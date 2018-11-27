da17c18d7bf7ba9227d3b61136a4e10b51c9641f
shogo.kitamura <shogo.kitamura@accenture.com>
2018-11-27 11:24:06 +0900

    Initial Commit

---
+++ multipeerConnectionSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  multipeerConnectionSample
+//
+//  Created by shogo.kitamura on 11/27/18.
+//  Copyright © 2018 shogo.kitamura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
